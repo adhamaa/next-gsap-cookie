c36ee1c3dbaf1d656705a946073ce209698d1ba2
adhamaa <adham_92@live.com>
2021-10-14 12:29:12 +0800

    Update nextconfig for local img to work

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@ const withTM = require("next-transpile-modules")(["gsap"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  images: {
+    // domains: ["images.unsplash.com"],
+    // path: "",
+    loader: "imgix",
+    path: "https://noop/",
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
